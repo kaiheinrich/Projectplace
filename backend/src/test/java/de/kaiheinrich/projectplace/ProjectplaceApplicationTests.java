7bc2d47bda9d914d4049460169a21f58a13996f0
Kai Heinrich <kai.heinrich91@web.de>
2020-11-16 10:37:36 +0100

    init spring server

---
+++ backend/src/test/java/de/kaiheinrich/projectplace/ProjectplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kaiheinrich.projectplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
