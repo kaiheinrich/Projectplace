47cbf2cdb38c6aa263e7b8dfd695076211ec0b81
Kai Heinrich <kai.heinrich91@web.de>
2020-11-27 12:39:38 +0100

    designed skills as tags and added to overview and detailspage

--- frontend/src/profiles/SkillList.js
+++ frontend/src/profiles/SkillList.js
@@ -1,15 +1,34 @@
 import React from "react";
+import styled from "styled-components/macro";
+import Chip from '@material-ui/core/Chip';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+    skill: {
+        margin: "4px",
+        backgroundColor: "#d7385e"
+    }
+}));
 
 export default function SkillList({skills}) {
+
+    const classes = useStyles();
 
     return(
         <>
             <h5>Skills:</h5>
-            <ul>
-                {skills?.map(skill =>
-                    <li key={skill}>{skill}</li>
+            <SkillListStyled>
+                {skills?.map((skill, index) =>
+                    <Chip className={classes.skill} label={skill} key={index}/>
                 )}
-            </ul>
+            </SkillListStyled>
         </>
     );
 }
+
+const SkillListStyled = styled.ul`
+  list-style: none;
+  padding: var(--size-m);
+  border: black 1px solid;
+`
+
