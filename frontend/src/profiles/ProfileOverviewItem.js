8a880219f49498afbba01c58a4abc1d84f6643b7
Kai Heinrich <kai.heinrich91@web.de>
2020-12-08 16:09:27 +0100

    change style of profile pages

--- frontend/src/profiles/ProfileOverviewItem.js
+++ frontend/src/profiles/ProfileOverviewItem.js
@@ -3,13 +3,10 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {useHistory} from "react-router-dom";
 import SkillList from "./SkillList";
-import {Avatar} from "@material-ui/core";
-import styled from "styled-components/macro";
-import Box from "@material-ui/core/Box";
+import {Avatar, CardHeader} from "@material-ui/core";
 
 export default function ProfileOverviewItem({profile}) {
 
@@ -17,25 +14,18 @@ export default function ProfileOverviewItem({profile}) {
     const history = useHistory();
 
     return (
-        <Card className={classes.root} variant="elevation">
+        <Card className={classes.root} variant="elevation" elevation={20}>
+            <CardHeader
+                className={classes.cardHeader}
+                title={profile.name}
+                subheader={profile.location}
+                action={profile.imageUrl ? <Avatar className={classes.avatar} src={profile.imageUrl}/> : <Avatar className={classes.avatar}/>}/>
             <CardContent>
-                <DivStyled>
-                    <div>
-                        <Typography variant="h5" component="h2">
-                            {profile.name}
-                        </Typography>
-                        <Typography className={classes.pos} color="textSecondary">
-                            Location: {profile.location}
-                        </Typography>
-                    </div>
-                    <Box component={'div'} className={classes.avatarPosition}>
-                        {profile.imageUrl && <Avatar className={classes.avatar} src={profile.imageUrl}/>}
-                    </Box>
-                </DivStyled>
                 <SkillList skills={profile.skills}/>
             </CardContent>
             <CardActions className={classes.buttonPosition}>
-                <Button className={classes.button} size="small" onClick={() => history.push(`/profile/${profile.username}`)}>Check out dude</Button>
+                <Button className={classes.button} size="small" onClick={() => history.push(`/profile/${profile.username}`)}>See more</Button>
+                <Button className={classes.button} size="small" onClick={() => history.push(`/messageto/${profile.username}`)}>Get in touch</Button>
             </CardActions>
         </Card>
     );
@@ -44,44 +34,27 @@ export default function ProfileOverviewItem({profile}) {
 const useStyles = makeStyles({
     root: {
         minWidth: 275,
-        backgroundColor: "#FFF4F4",
-        borderRadius: "20px"
+        backgroundColor: "#FFFFFF",
+        borderRadius: "5px"
+    },
+    cardHeader: {
+        backgroundColor: "#F3EED9"
     },
     button: {
-        backgroundColor: "#9F0D2F",
-        color: "white",
+        backgroundColor: "#e7e7e7",
+        color: "black",
+        width: "150px",
         borderRadius: "10px",
         fontSize: "0.9em",
         padding: "8px"
     },
     buttonPosition: {
-        justifyContent: "center"
+        justifyContent: "center",
+        alignContent: "center",
+        padding: "0px 16px 16px 16px"
     },
     avatar: {
-        width: "60px",
-        height: "60px"
-    },
-    name: {
-        fontSize: 20
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 20,
-    },
-    avatarPosition: {
-        alignSelf: "start",
-        justifySelf: "end"
+        width: "70px",
+        height: "70px"
     }
 });
-
-const DivStyled = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-`
