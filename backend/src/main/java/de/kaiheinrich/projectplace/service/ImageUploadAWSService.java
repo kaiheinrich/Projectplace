f2f5f8596051ee0a9c50910f773bde9d9a48a644
Kai Heinrich <kai.heinrich91@web.de>
2020-12-10 12:23:57 +0100

    image urls are saved as random strings in s3 bucket

--- backend/src/main/java/de/kaiheinrich/projectplace/service/ImageUploadAWSService.java
+++ backend/src/main/java/de/kaiheinrich/projectplace/service/ImageUploadAWSService.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import de.kaiheinrich.projectplace.utils.AmazonS3ClientUtils;
+import de.kaiheinrich.projectplace.utils.IdUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,22 +21,25 @@ public class ImageUploadAWSService {
     private String bucketName;
 
     private final AmazonS3ClientUtils s3ClientUtils;
+    private final IdUtils idUtils;
 
     @Autowired
-    public ImageUploadAWSService(AmazonS3ClientUtils s3ClientUtils) {
+    public ImageUploadAWSService(AmazonS3ClientUtils s3ClientUtils, IdUtils idUtils) {
         this.s3ClientUtils = s3ClientUtils;
+        this.idUtils = idUtils;
     }
 
-    public String upload(MultipartFile file) throws IOException, InterruptedException {
+    public String upload(MultipartFile file) throws IOException {
 
         AmazonS3 s3Client = s3ClientUtils.getS3Client();
+        String randomFileName = idUtils.generateId();
 
         try {
             ObjectMetadata metadata = new ObjectMetadata();
-            PutObjectRequest request = new PutObjectRequest(bucketName, file.getOriginalFilename(), file.getInputStream(), metadata);
+            PutObjectRequest request = new PutObjectRequest(bucketName, randomFileName, file.getInputStream(), metadata);
             s3Client.putObject(request);
 
-            return file.getOriginalFilename();
+            return randomFileName;
 
         } catch (AmazonServiceException e) {
             e.printStackTrace();
